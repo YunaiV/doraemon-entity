4421e0ac41a4b192cffb2c2c5433a22ead8cca10
YunaiV <>
2018-02-07 15:48:53 +0800

    交易退款

---
+++ src/main/java/cn/iocoder/doraemon/itemgroup/item/ItemCategoryAPI.java
@@ -0,0 +1,21 @@
+package cn.iocoder.doraemon.itemgroup.item;
+
+import com.youzan.open.sdk.gen.v3_0_0.model.YouzanItemcategoriesGetResult;
+
+import java.util.List;
+
+/**
+ * 商品类目 API
+ */
+public interface ItemCategoryAPI {
+
+    /**
+     * 获取商品类目列表
+     *
+     * https://www.youzanyun.com/apilist/detail/group_item/item_category/youzan.itemcategories.get
+     *
+     * @return 商品类目列表
+     */
+    List<YouzanItemcategoriesGetResult.GoodsCategory> list();
+
+}
--- src/main/java/cn/iocoder/doraemon/itemgroup/item/ItemTagAPI.java
+++ src/main/java/cn/iocoder/doraemon/itemgroup/item/ItemTagAPI.java
@@ -1,18 +1,69 @@
 package cn.iocoder.doraemon.itemgroup.item;
 
+import com.youzan.open.sdk.gen.v3_0_0.model.YouzanItemcategoriesTagAddResult;
+import com.youzan.open.sdk.gen.v3_0_0.model.YouzanItemcategoriesTaglistSearchResult;
+import com.youzan.open.sdk.gen.v3_0_0.model.YouzanItemcategoriesTagsGetResult;
+
+import java.util.List;
+
 /**
  * 商品分组 API
  */
-public class ItemTagAPI {
-
-    // 新增商品分组
-
-    // 更新商品分组
-
-    // 删除商品分组
+public interface ItemTagAPI {
 
     // 获得单条商品分组
 
-    // 搜索商品分组
+    /**
+     * 分页获取商品分组列表
+     *
+     * https://www.youzanyun.com/apilist/detail/group_item/item_category/youzan.itemcategories.taglist.search
+     *
+     * @param orderBy 排序方式。格式为column:asc/desc，column可选值：created 创建时间
+     * @param pageNo 页码
+     * @param pageSize 每页条数
+     * @return 分组列表
+     */
+    YouzanItemcategoriesTaglistSearchResult search(String orderBy, Integer pageNo, Integer pageSize);
+
+    /**
+     * 根据是否排序查询商品分组列表
+     *
+     * https://www.youzanyun.com/apilist/detail/group_item/item_category/youzan.itemcategories.tags.get
+     *
+     * @param isSort 是否排序
+     * @return 分组列表信息
+     */
+    List<YouzanItemcategoriesTagsGetResult.ItemGroupOpenModel> list(Boolean isSort);
+
+    /**
+     * 新增一个商品分组
+     *
+     * https://www.youzanyun.com/apilist/detail/group_item/item_category/youzan.itemcategories.tag.add
+     *
+     * @param name 商品分组的名称
+     * @return 商品分组信息
+     */
+    YouzanItemcategoriesTagAddResult add(String name);
+
+    /**
+     * 更新一个商品分组名
+     *
+     * https://www.youzanyun.com/apilist/detail/group_item/item_category/youzan.itemcategories.tag.update
+     *
+     * @param tagId 商品分组编号
+     * @param name 商品分组的名称
+     * @return 是否成功
+     */
+    Boolean update(String tagId, String name);
+
+    /**
+     * 删除一个商品分组
+     *
+     * https://www.youzanyun.com/apilist/detail/group_item/item_category/youzan.itemcategories.tag.delete
+     *
+     * @param tagId 商品分组编号
+     * @return 是否成功
+     */
+    Boolean delete(String tagId);
 
 }
